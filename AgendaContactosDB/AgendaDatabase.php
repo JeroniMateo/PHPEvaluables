ecb9960c3509410dba4fc4d365f2e0da9cc8d2da
Jeroni <jmateo@cifpfbmoll.eu>
2021-12-09 11:06:12 +0100

    insert query

--- AgendaContactosDB/AgendaDatabase.php
+++ AgendaContactosDB/AgendaDatabase.php
@@ -1,12 +1,29 @@
 <?php
 
 
+require("database.php");
+
+$database = new Database();
+$db = $database->getConnection();
+
+$showAgenda = $db->query("select * from agenda");
+$nombre = $_POST['name'];
+$telefono = $_POST['telf'];
+$showAgenda =  $db->query('insert into agenda(nombre,telefono) values ($nombre,$telefono) ');
+
+echo "<table> <th> Agenda de Contactos </th>";
+foreach ($showAgenda as $row) {
+    echo "<tr> <td>" .  $row['nombre'] . "</td>" ;
+   
+    echo "<td>" . $row['telefono'] .  "</td></tr>";
+}
+echo "</table>"
 
 
 ?>
-
-<form action="">
-    <label>Nombre:<input type="text" name="nombre" /></label><br />
-    <label>Teléfono:<input type="number" name="telefono" /></label><br />
+<form action="agendacheck.php" method="POST">
+    <label>Nombre:<input type="text" name="name"   /></label><br />
+    <label>Teléfono:<input type="number" name="telf"  /></label><br />
     <input type="submit" name='submit' value="Ejecutar" /><br />
 </form>
+
