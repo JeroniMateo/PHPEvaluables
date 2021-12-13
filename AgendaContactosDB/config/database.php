680ddc72f6e2087bc275ac257a720ee4048f9bcb
Jeroni <jmateo@cifpfbmoll.eu>
2021-12-13 20:49:43 +0100

    Postgre DB

--- AgendaContactosDB/config/database.php
+++ AgendaContactosDB/config/database.php
@@ -3,10 +3,10 @@
 class Database{
  
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "agendadatabase";
-    private $username = "root";
-    private $password = "password";
+    private $host = "51.178.152.213";
+    private $db_name = "jmateo_agenda_db";
+    private $username = "jmate_usr";
+    private $password = "abc123.";
     public $conn;
  
     // get the database connection
@@ -15,7 +15,7 @@ class Database{
         $this->conn = null;
  
         try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
--- AgendaContactosDB/pdoDB.php
+++ AgendaContactosDB/pdoDB.php
@@ -1,12 +1,12 @@
 <?php
 //selecionamos los nombres de la agenda
-$stmt = $pdo->query('SELECT nombre FROM agenda');
+$stmt = $pdo->query('SELECT nombre FROM jmateo_agenda_db');
 while ($row = $stmt->fetch())
 {
     echo $row['nombre'] . "\n";
 }
 //selecionamos los telefonos de la agenda
-$stmt = $pdo->query('SELECT telefono FROM agenda');
+$stmt = $pdo->query('SELECT telefono FROM jmateo_agenda_db');
 while ($row = $stmt->fetch())
 {
     echo $row['telefono'] . "\n";
